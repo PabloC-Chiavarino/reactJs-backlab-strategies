3b5bca8eb151d23b90fe34c5e7c1156e9e4c62f7
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2024-03-02 19:51:01 -0300

    fix(web): correction on whatsapp button redirection

--- src/components/WhatsappButton/index.jsx
+++ src/components/WhatsappButton/index.jsx
@@ -4,7 +4,7 @@ import './styles.css'
 const WhatsAppButton = () => {
     return (
         <div className="whatsapp-button">
-            <a href="whatsapp://tu_numero_de_telefono" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/5492281300940" target="_blank" rel="noopener noreferrer">
                 <img className='whatsapp-icon' src={whatsapp} alt="WhatsApp Icon"/>
             </a>
         </div>
