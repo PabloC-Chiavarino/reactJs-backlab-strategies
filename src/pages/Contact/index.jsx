fa64b2320dd4750455d92298f403f0c16b97cfa5
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2024-03-14 20:47:53 -0300

    feat(Contact): changed to controlled form to enable/disable submit button

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,18 +1,40 @@
+import { useState, useRef } from 'react'
 import { Bounce, ToastContainer, toast } from 'react-toastify'
 import emailjs from '@emailjs/browser'
-import { useRef } from 'react'
 import { Faq } from '../../components'
 import { contactImg, socialMedia } from '../../assets/img'
 import './styles.css'
 import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
+    
+    const form = useRef()
 
-    const formData = useRef()
+    const [ formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const [isDisabled, setIsDisabled] = useState(true)
+
+    const handleOnChange = (e) => {
+
+        formData.name && formData.email && formData.message !== '' ?
+            setIsDisabled(false)
+            :
+            setIsDisabled(true)
+            
+        setFormData(
+            {
+                ...formData,
+                [e.target.name]: e.target.value
+            }
+        )
+    }
 
     const throwSuccessPopUp = () => {
         toast.success('Formulario enviado correctamente!', {
-            // position: "top-right",
             autoClose: 4000,
             hideProgressBar: false,
             closeOnClick: true,
@@ -44,15 +66,33 @@ const Contact = () => {
             <ToastContainer />
             <div className='form-container gradient-border'>
 
-                <form ref={formData} onSubmit={sendEmail}>
+                <form ref={form} onSubmit={sendEmail}>
                     <span>
                         <div>
                             <label htmlFor="name">Nombre</label>
-                            <input id='name' type="text" placeholder='Juan' name='user_name' required autoComplete='on' />
+                            <input
+                                id='name'
+                                type="text"
+                                placeholder='Juan'
+                                name='name'
+                                required
+                                autoComplete='on'
+                                onChange={handleOnChange}
+                                value={formData.name}
+                            />
                         </div>
                         <div>
                             <label htmlFor="email">Email</label>
-                            <input id='email' type="text" placeholder='juan@gmail.com' name='user_email' required autoComplete='on' />
+                            <input
+                                id='email'
+                                type="text"
+                                placeholder='juan@gmail.com'
+                                name='email'
+                                required
+                                autoComplete='on'
+                                onChange={handleOnChange}
+                                value={formData.email}
+                            />
                         </div>
                     </span>
                     <span>
@@ -60,26 +100,65 @@ const Contact = () => {
                             <h4 htmlFor="">¿Tenés experiencia en trading?</h4>
                             <span>
                                 <label htmlFor="tExpY">Sí</label>
-                                <input id='tExpY' type="radio" name='user_tradingExp' value='Sí' required />
+                                <input
+                                    id='tExpY'
+                                    type="radio"
+                                    name='user_tradingExp'
+                                    value='Sí'
+                                    required
+                                />
                                 <label htmlFor="tExpN">No</label>
-                                <input id='tExpN' type="radio" name='user_tradingExp' value='No' required />
+                                <input
+                                    id='tExpN'
+                                    type="radio"
+                                    name='user_tradingExp'
+                                    value='No'
+                                    required
+                                />
                             </span>
                         </div>
                         <div>
                             <h4>¿Cuántos activos te gustaría testear?</h4>
                             <span>
                                 <label htmlFor='actives1'>1-3</label>
-                                <input id='actives1' type="radio" name='user_actives' value='1-3' required />
+                                <input
+                                    id='actives1'
+                                    type="radio"
+                                    name='user_actives'
+                                    value='1-3'
+                                    required
+                                />
                                 <label htmlFor='actives2'>4-10</label>
-                                <input id='actives2' type="radio" name='user_actives' value='4-10' required />
+                                <input
+                                    id='actives2'
+                                    type="radio"
+                                    name='user_actives'
+                                    value='4-10'
+                                    required
+                                />
                                 <label htmlFor='actives3'>+10</label>
-                                <input id='actives3' type="radio" name='user_actives' value='10+' required />
+                                <input
+                                    id='actives3'
+                                    type="radio"
+                                    name='user_actives'
+                                    value='10+'
+                                    required
+                                />
                             </span>
                         </div>
                     </span>
                     <label htmlFor='message'>Tu mensaje</label>
-                    <textarea id='message' name="user_message" minLength="20" cols="" rows="" placeholder='Mensaje' required />
-                    <button type='submit' className='submit-button'>Enviar</button>
+                    <textarea
+                        id='message'
+                        name="message"
+                        minLength="20" cols=""
+                        rows=""
+                        placeholder='Mensaje'
+                        required
+                        onChange={handleOnChange}
+                        value={formData.message}
+                    />
+                    <button type='submit' className= {isDisabled ? 'submit-button disabled' : 'submit-button'} disabled={isDisabled} >Enviar</button>
                 </form>
                 <div className="form-subcontainer">
                     <img className='gradient-border-bottom' style={{ height: '556px' }} src={contactImg} alt="" />
