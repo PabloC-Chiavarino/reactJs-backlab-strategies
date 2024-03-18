dc76fbcd928ae1464d1ebf8d905689430e50df74
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2024-03-18 20:51:54 -0300

    Info(About us): add rest of content on personal cards

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -43,14 +43,14 @@ const About = () => {
                         <img src={team1} alt="" />
                         <div className='team-data'>
                             <h3 className='team-name'>Juan Casimiro</h3>
-                            <h6>ROL</h6>
+                            <h6>Desarrollador</h6>
                             <p className='team-detail' >
                                 ¡Hola a todos! Soy Juan Casimiro, y entiendo los desafíos y preocupaciones que enfrentan los traders al tratar de navegar por los mercados financieros. Como trader autodidacta y desarrollador full stack, he dedicado mi carrera a crear soluciones tecnológicas que faciliten y mejoren la experiencia de trading. Como trader autodidacta durante más de cuatro años, conozco la importancia de contar con herramientas confiables y efectivas para tomar decisiones informadas en un mercado volátil.
-                                <br/>
-                                <br/>
+                                <br />
+                                <br />
                                 Entiendo los miedos y las incertidumbres que pueden surgir al enfrentarse al mundo del trading, y estoy aquí para ofrecerte las herramientas y el apoyo que necesitas para tener éxito. Con un enfoque en la profesionalidad, la calidad y la transparencia, mi objetivo es proporcionarte soluciones personalizadas que se adapten a tus necesidades. Ya sea que estés buscando optimizar tus estrategias de trading o simplificar tus procesos de análisis, estoy aquí para ayudarte a alcanzar tus objetivos financieros.
-                                <br/>
-                                <br/>
+                                <br />
+                                <br />
                                 ¡No estás solo en este viaje! Estoy aquí para ofrecerte apoyo y orientación en cada paso del camino.
                             </p>
                         </div>
@@ -58,19 +58,13 @@ const About = () => {
                     <div className='team' style={{ left: '6rem' }}>
                         <img src={team2} alt="" />
                         <div className='team-data'>
-                            <h3 className='team-name'>Nombre del integrante</h3>
-                            <h6>ROL</h6>
+                            <h3 className='team-name'>Ignacio Tolosa</h3>
+                            <h6>Analista</h6>
                             <p className='team-detail' >
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
-                                Lorem Impsum Lorem Impsum Lorem Impsum
+                                ¡Bienvenidos traders! Soy Ignacio Tolosa, desarrollador y analista de datos en BackLab. Entiendo perfectamente los desafíos que enfrentan los traders al operar en los mercados financieros y encontrar estrategias rentables. Como desarrollador y trader autodidacta, me he dedicado a crear soluciones tecnológicas que simplifiquen y mejoren el proceso de backtesting.
+                                <br />
+                                <br />
+                                Durante mi carrera, he trabajado arduamente para desarrollar herramientas de backtesting confiables y efectivas que permitan a nuestros clientes tomar decisiones informadas en un mercado tan dinámico como el financiero. En mi papel como socio de BackLab, me comprometo a ofrecer soluciones personalizadas que se adapten a las necesidades específicas de nuestros clientes.
                             </p>
                         </div>
                     </div>
